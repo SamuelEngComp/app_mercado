3f4d8fd129cbab25753456f7112e84478ef2ce56
SamuelEngComp <samuellima810@gmail.com>
2022-02-27 09:59:19 -0300

    adicionei um comentario na class produto

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,11 +1,17 @@
+/**
+ * ESSA MODELAGEM SÓ SERVE PARA COMPRAS EM UNIDADES, DEVO MODELAR NOVAMENTE
+ * PARA OS CASOS EM QUE O VALOR DO PRODUTO É CALCULADO PELO KG
+ *
+ */
+
 class Product{
   String name;
   double quantity;
-  double value;
+  double valueUnit;
 
   Product({
     required this.name,
     required this.quantity,
-    required this.value});
+    required this.valueUnit});
 
 }
